48c535e5fb1cbfbefde847ed9263c0d5e8dd14b8
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2023-11-13 21:06:29 +0200

    Aiempi moka korjattu

--- viikko2/src/index.py
+++ viikko2/src/index.py
@@ -1,8 +1,3 @@
 import cowsay
 
 cowsay.cow('Laitetaan VS Code toimimaan kunnolla!')
-<<<<<<< HEAD
-print("Terveiset kloonista 2")
-=======
-pass
->>>>>>> 2d01a16 (Muokkaus paikallisesta repositoriosta 1)
