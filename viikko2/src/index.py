c2def1cb062045c5d76f43a229ca8086013f3f5c
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2023-11-13 20:59:59 +0200

    Merge-konfliktin ratkaisu

--- viikko2/src/index.py
+++ viikko2/src/index.py
@@ -1,4 +1,8 @@
 import cowsay
 
 cowsay.cow('Laitetaan VS Code toimimaan kunnolla!')
+<<<<<<< HEAD
 print("Terveiset kloonista 2")
+=======
+pass
+>>>>>>> 2d01a16 (Muokkaus paikallisesta repositoriosta 1)
